280c4fb1fe8ec58e5a03edc2fe959ab546cec160
Anders K. Pettersen <staticaland@users.noreply.github.com>
2023-11-17 23:04:08 +0100

    feat: Make _config.auto.tfvars.json from env.yml

--- pkg/bootstrap/anders_experiment.go
+++ pkg/bootstrap/anders_experiment.go
@@ -1,6 +1,7 @@
 package bootstrap
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"os"
@@ -11,7 +12,8 @@ import (
 	"gopkg.in/yaml.v3"
 )
 
-type Config struct {
+// OriginalConfig represents the structure of the YAML configuration file (think of `env.yml`)“.
+type OriginalConfig struct {
 	APIVersion string `yaml:"apiVersion"`
 	Kind       string `yaml:"kind"`
 	Metadata   struct {
@@ -24,13 +26,52 @@ type Config struct {
 	} `yaml:"aws"`
 }
 
-func load() Config {
+// JSONConfig represents the structure of the JSON configuration file (think of `_config.auto.tfvars.json`).
+type JSONConfig struct {
+	AccountID   string `json:"account_id"`
+	Region      string `json:"region"`
+	TeamName    string `json:"team_name"`
+	Environment string `json:"environment"`
+}
+
+// ToJSONFile writes the JSONConfig to a file in JSON format.
+// filename: Name of the file to write the JSON data to.
+// Returns an error if file creation or JSON encoding fails.
+func (c *JSONConfig) ToJSONFile(filename string) error {
+	file, err := os.Create(filename)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	encoder := json.NewEncoder(file)
+	encoder.SetIndent("", "    ")
+
+	return encoder.Encode(c)
+}
+
+// TranslateToJSONConfig converts an OriginalConfig object to a JSONConfig object.
+// orig: Pointer to the OriginalConfig to be converted.
+// Returns a pointer to the new JSONConfig object.
+func TranslateToJSONConfig(orig *OriginalConfig) *JSONConfig {
+	return &JSONConfig{
+		AccountID:   orig.AWS.AccountID,
+		Region:      orig.AWS.Region,
+		TeamName:    orig.Metadata.Team,
+		Environment: orig.Metadata.Environment,
+	}
+}
+
+// load reads and unmarshals a YAML file into an OriginalConfig object.
+// Returns the unmarshalled OriginalConfig.
+// Exits the program if reading or unmarshalling fails.
+func load() OriginalConfig {
 	data, err := os.ReadFile("env.yml")
 	if err != nil {
 		log.Fatalf("error reading file: %v", err)
 	}
 
-	var config Config
+	var config OriginalConfig
 	err = yaml.Unmarshal(data, &config)
 	if err != nil {
 		log.Fatalf("error unmarshaling YAML: %v", err)
@@ -39,6 +80,10 @@ func load() Config {
 	return config
 }
 
+// readHCLFile reads and parses an HCL file.
+// filename: Name of the HCL file to be read.
+// Returns a pointer to the parsed hclwrite.File and an error if any.
+// Errors are reported if file reading or parsing fails.
 func readHCLFile(filename string) (*hclwrite.File, error) {
 	content, err := terraformFile.ReadFile(filename)
 	if err != nil {
@@ -53,9 +98,17 @@ func readHCLFile(filename string) (*hclwrite.File, error) {
 	return file, nil
 }
 
+// Entry is the main entry point of the application logic.
+// It orchestrates the loading, translating, and writing of configuration files.
 func Entry() {
 
-	//config := load()
+	origConfig := load()
+	jsonConfig := TranslateToJSONConfig(&origConfig)
+
+	err := jsonConfig.ToJSONFile("_config.auto.tfvars.json")
+	if err != nil {
+		fmt.Println("Error writing JSON file:", err)
+	}
 
 	tfFile, err := readHCLFile("example.tf")
 	if err != nil {
@@ -68,6 +121,9 @@ func Entry() {
 
 }
 
+// modifyTfFile modifies a Terraform (HCL) file.
+// tfFile: Pointer to the Terraform file to be modified.
+// Returns the modified hclwrite.File.
 func modifyTfFile(tfFile *hclwrite.File) *hclwrite.File {
 
 	localsBlock := tfFile.Body().FirstMatchingBlock("locals", nil)
@@ -78,6 +134,10 @@ func modifyTfFile(tfFile *hclwrite.File) *hclwrite.File {
 	return tfFile
 }
 
+// writeHCLFile writes the modified HCL content to a file.
+// file: Pointer to the hclwrite.File containing the HCL content.
+// filename: Name of the file to write the modified HCL content to.
+// Returns an error if writing the file fails.
 func writeHCLFile(file *hclwrite.File, filename string) error {
 	modifiedContent := file.Bytes()
 	err := os.WriteFile(filename, modifiedContent, 0644)

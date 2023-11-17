12014491473bb27b24252c9891eee1574b345ed8
Yngvar Kristiansen <562343+yngvark@users.noreply.github.com>
2023-11-17 13:45:45 +0100

    fix: Use bootstrap as arg

--- cmd/bootstrap.go
+++ cmd/bootstrap.go
@@ -13,7 +13,7 @@ var bootstrapCommand = &cobra.Command{
 	Use:   "bootstrap",
 	Short: "This command will create the necessary S3 bucket and DynamoDB table that will be used to store Terraform state.",
 	Run: func(cmd *cobra.Command, args []string) {
-		fullArgs := append([]string{"scaffold"}, args...)
+		fullArgs := append([]string{"bootstrap"}, args...)
 		scriptrunner.RunScript("ok.sh", fullArgs)
 	},
 }

fee2dcdeb8614512f035e4a810b30468b247a2a5
Yngvar Kristiansen <562343+yngvark@users.noreply.github.com>
2024-06-19 13:48:25 +0200

    chore: Add zerolog

--- main.go
+++ main.go
@@ -2,8 +2,13 @@ package main
 
 import (
 	"github.com/oslokommune/ok/cmd"
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+	"os"
 )
 
 func main() {
+	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stdout})
+
 	cmd.Execute()
 }

3c7cb840a571cb7e855cbeb5510f79ca36dbc5ca
Yngvar Kristiansen <562343+yngvark@users.noreply.github.com>
2024-07-17 08:43:51 +0200

    chore: Improve --help for pkg install

--- cmd/pkg/install.go
+++ cmd/pkg/install.go
@@ -6,8 +6,18 @@ import (
 )
 
 var InstallCommand = &cobra.Command{
-	Use:           "install [stackName1, stackName2, ...]",
-	Short:         "Install or update Boilerplate packages",
+	Use:   "install [outputFolder ...]",
+	Short: "Install or update Boilerplate packages",
+	Long: `Install or update Boilerplate packages.
+
+If no arguments are used, the command installs all the packages specified in the package manifest file.
+
+If one or more output folders are specified, the command installs only the packages from the package manifest that has
+OutputFolder that matches one of the specified output folders.
+`,
+	Example: `ok install networking
+ok install networking my-app
+`,
 	SilenceErrors: true,
 	RunE: func(cmd *cobra.Command, args []string) error {
 		err := install.Run(args)

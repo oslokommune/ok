9345202b28fa27b839d06d89ca4e9ddcdce2df44
Yngvar Kristiansen <562343+yngvark@users.noreply.github.com>
2024-07-22 12:30:09 +0200

    feat: Add help text for BASE_URL

--- cmd/pkg/install.go
+++ cmd/pkg/install.go
@@ -13,9 +13,12 @@ var InstallCommand = &cobra.Command{
 If no arguments are used, the command installs all the packages specified in the package manifest file.
 
 If one or more output folders are specified, the command installs only the packages whose OutputFolder matches the specified folders. (OutputFolder is a field in the package manifest file.)
+
+Set the environment variable BASE_URL to specify where package templates are downloaded from. 
 `,
 	Example: `ok install networking
 ok install networking my-app
+BASE_URL=../boilerplate/terraform ok install networking my-app
 `,
 	SilenceErrors: true,
 	RunE: func(cmd *cobra.Command, stacks []string) error {

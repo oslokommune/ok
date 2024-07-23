43a3c6e27068ae6335ec1b47b5a41d69403c5f45
Yngvar Kristiansen <562343+yngvark@users.noreply.github.com>
2024-07-23 14:23:07 +0200

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:pkg/pkg/list/list.go

--- pkg/pkg/list/list.go
+++ pkg/pkg/list/list.go
@@ -6,11 +6,6 @@ import (
 	"github.com/oslokommune/ok/pkg/pkg/update"
 )
 
-type Release struct {
-	Component string
-	Version   string
-}
-
 func Run(pkgManifestFilename string) error {
 	manifest, err := update.LoadPackageManifest(pkgManifestFilename)
 	if err != nil {

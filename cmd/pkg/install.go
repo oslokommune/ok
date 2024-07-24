c83725007e194c1b339506cd4a2087eab9ba3b26
Yngvar Kristiansen <562343+yngvark@users.noreply.github.com>
2024-07-24 15:33:24 +0200

    feat: Add multi select (#170)

--- cmd/pkg/install.go
+++ cmd/pkg/install.go
@@ -2,15 +2,17 @@ package pkg
 
 import (
 	"fmt"
-	"github.com/oslokommune/ok/pkg/pkg/install"
 	"github.com/oslokommune/ok/pkg/pkg/install/interactive"
+
+	"github.com/oslokommune/ok/pkg/pkg/install"
 	"github.com/spf13/cobra"
 )
 
 var flagInstallInteractive bool
 
 func init() {
-	InstallCommand.Flags().BoolVarP(&flagInstallInteractive, "interactive", "i", false, "Select package interactively")
+	InstallCommand.Flags().BoolVarP(&flagInstallInteractive,
+		"interactive", "i", false, "Select package(s) to install interactively")
 }
 
 var InstallCommand = &cobra.Command{
@@ -22,31 +24,35 @@ If no arguments are used, the command installs all the packages specified in the
 
 If one or more output folders are specified, the command installs only the packages whose OutputFolder matches the specified folders. (OutputFolder is a field in the package manifest file.)
 
-Set the environment variable BASE_URL to specify where package templates are downloaded from. 
+Set the environment variable BASE_URL to specify where package templates are downloaded from.
 `,
 	Example: `ok install networking
 ok install networking my-app
 BASE_URL=../boilerplate/terraform ok install networking my-app
 `,
 	SilenceErrors: true,
-	RunE: func(cmd *cobra.Command, stacks []string) error {
+	RunE: func(cmd *cobra.Command, outputFolders []string) error {
+		if flagInstallInteractive && len(outputFolders) > 0 {
+			return fmt.Errorf("cannot use both --interactive and outputFolder arguments")
+		}
+
 		if flagInstallInteractive {
-			answer, err := interactive.Run(PackagesManifestFilename)
+			selectedOutputFolders, err := interactive.SelectPackagesToInstall(PackagesManifestFilename)
 			if err != nil {
 				return fmt.Errorf("selecting package: %w", err)
 			}
 
-			if answer.Aborted {
-				fmt.Println("Aborted")
+			if len(selectedOutputFolders) == 0 {
+				fmt.Println("No packages selected. Remember to use space (or x) to select package(s) to install.")
 				return nil
 			}
 
-			stacks = []string{answer.Choice}
+			outputFolders = selectedOutputFolders
 		}
 
-		err := install.Run(PackagesManifestFilename, stacks)
+		err := install.Run(PackagesManifestFilename, outputFolders)
 		if err != nil {
-			return err
+			return fmt.Errorf("installing packages: %w", err)
 		}
 
 		return nil

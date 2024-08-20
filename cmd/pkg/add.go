d080062ba7cb94fd95e989977a19598747a6c6d8
Jarle Fosen <jarlefosen@gmail.com>
2024-08-20 08:48:52 +0200

    fix: rename stack-name to output-folder and print missing config files

--- cmd/pkg/add.go
+++ cmd/pkg/add.go
@@ -1,32 +1,41 @@
 package pkg
 
 import (
+	"errors"
+	"os"
+
 	"github.com/oslokommune/ok/pkg/pkg/add"
 	"github.com/spf13/cobra"
 )
 
-var flagAddOutputFolder string
-
 var AddCommand = &cobra.Command{
-	Use:   "add template [stack-name]",
-	Short: "Add Boilerplate template to packages manifest with an optional stack name",
-	Long: `Add Boilerplate template to packages manifest with an optional stack name.
+	Use:   "add template [outputFolder]",
+	Short: "Add Boilerplate template to packages manifest with an optional output folder",
+	Long: `Add Boilerplate template to packages manifest with an optional output folder.
 The template version is fetched from the latest release on GitHub and added to the packages manifest without applying the template.
-The stack name is useful to define if you need multiple instances of the same template with different configurations. For example having multiple apps in the same project.`,
+The output folder is useful to define if you need multiple instances of the same template with different configurations. For example having multiple apps in the same project.`,
 	Example: `ok pkg add databases my-postgres-database
-ok pkg add app website --output-folder ecommerce
-ok pkg add app api --output-folder ecommerce
+ok pkg add app ecommerce-website
+ok pkg add app ecommerce-api
 	`,
 	Args: cobra.RangeArgs(1, 2),
 	RunE: func(cmd *cobra.Command, args []string) error {
 		templateName := getArg(args, 0, "")
-		stackName := getArg(args, 1, templateName)
+		outputFolder := getArg(args, 1, templateName)
 
-		err := add.Run(PackagesManifestFilename, templateName, flagAddOutputFolder, stackName)
+		result, err := add.Run(PackagesManifestFilename, templateName, outputFolder)
 		if err != nil {
 			return err
 		}
 
+		cmd.Printf("%s (%s) added to %s with output folder name %s\n", result.TemplateName, result.TemplateVersion, PackagesManifestFilename, result.OutputFolder)
+		nonExistingConfigFiles := findNonExistingConfigurationFiles(result.VarFiles)
+		if len(nonExistingConfigFiles) > 0 {
+			cmd.Printf("\nCreate these following configuration files:\n")
+			for _, configFile := range nonExistingConfigFiles {
+				cmd.Printf("- %s\n", configFile)
+			}
+		}
 		return nil
 	},
 }
@@ -38,6 +47,14 @@ func getArg(args []string, index int, fallback string) string {
 	return fallback
 }
 
-func init() {
-	AddCommand.Flags().StringVarP(&flagAddOutputFolder, "output-folder", "o", "", "Output folder for the new package. Defaults to the current working directory.")
+func findNonExistingConfigurationFiles(varFiles []string) []string {
+	var nonExisting []string
+	for _, varFile := range varFiles {
+		_, err := os.Stat(varFile)
+		notExists := errors.Is(err, os.ErrNotExist)
+		if notExists {
+			nonExisting = append(nonExisting, varFile)
+		}
+	}
+	return nonExisting
 }
--- pkg/pkg/add/add.go
+++ pkg/pkg/add/add.go
@@ -2,34 +2,40 @@ package add
 
 import (
 	"fmt"
-	"path/filepath"
 
 	"github.com/oslokommune/ok/pkg/pkg/common"
 	"github.com/oslokommune/ok/pkg/pkg/githubreleases"
 )
+
+type AddResult struct {
+	OutputFolder    string
+	VarFiles        []string
+	TemplateName    string
+	TemplateVersion string
+}
 
 /**
  * Add Boilerplate template to packages manifest with an optional stack name.
  * The template version is fetched from the latest release on GitHub and added to the packages manifest without applying the template.
  * The output folder is prefixed with the stack name and added to the packages manifest.
  */
-func Run(pkgManifestFilename string, templateName, outputFolderName, stackName string) error {
+func Run(pkgManifestFilename string, templateName, outputFolder string) (*AddResult, error) {
 
 	latestReleases, err := githubreleases.GetLatestReleases()
 	if err != nil {
-		return fmt.Errorf("failed getting latest github releases: %w", err)
+		return nil, fmt.Errorf("failed getting latest github releases: %w", err)
 	}
 
 	templateVersion := latestReleases[templateName]
 	if templateVersion == "" {
-		return fmt.Errorf("template %s not found in latest releases", templateName)
+		return nil, fmt.Errorf("template %s not found in latest releases", templateName)
 	}
 
-	outputFolder := filepath.Join(outputFolderName, stackName)
 	varFiles := []string{
 		"_config/common-config.yml",
-		fmt.Sprintf("_config/%s.yml", stackName),
+		fmt.Sprintf("_config/%s.yml", outputFolder),
 	}
+
 	newPackage := common.Package{
 		Template:     templateName,
 		Ref:          fmt.Sprintf("%s-%s", templateName, templateVersion),
@@ -39,15 +45,25 @@ func Run(pkgManifestFilename string, templateName, outputFolderName, stackName s
 
 	manifest, err := common.LoadPackageManifest(pkgManifestFilename)
 	if err != nil {
-		return err
+		return nil, err
 	}
 
 	for _, pkg := range manifest.Packages {
 		if pkg.OutputFolder == newPackage.OutputFolder {
-			return fmt.Errorf("output folder %s already exists in manifest", newPackage.OutputFolder)
+			return nil, fmt.Errorf("output folder %s already exists in manifest", newPackage.OutputFolder)
 		}
 	}
 
 	manifest.Packages = append(manifest.Packages, newPackage)
-	return common.SavePackageManifest(pkgManifestFilename, manifest)
+	err = common.SavePackageManifest(pkgManifestFilename, manifest)
+	if err != nil {
+		return nil, err
+	}
+
+	return &AddResult{
+		OutputFolder:    outputFolder,
+		VarFiles:        varFiles,
+		TemplateName:    templateName,
+		TemplateVersion: templateVersion,
+	}, nil
 }

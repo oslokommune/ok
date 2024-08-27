7194e3ff93c72f0c02ceaad7ba94f0e7e4d7805b
Anders K. Pettersen <staticaland@users.noreply.github.com>
2024-08-27 10:58:14 +0200

    feat: Allow updating single package with `ok pkg update <package-name>`

--- cmd/pkg/update.go
+++ cmd/pkg/update.go
@@ -1,19 +1,36 @@
 package pkg
 
 import (
+	"fmt"
+
 	"github.com/oslokommune/ok/pkg/pkg/update"
 	"github.com/spf13/cobra"
 )
 
 var UpdateCommand = &cobra.Command{
-	Use:           "update",
-	Short:         "Update Boilerplate package manifest.",
-	Example:       `ok pkg update`,
-	SilenceErrors: true,
+	Use:   "update [package-name]",
+	Short: "Update Boilerplate package manifest",
+	Long: `Update Boilerplate package manifest.
+If a package name is provided, only that package will be updated.
+If no package name is provided, all packages will be updated.`,
+	Example: `  ok pkg update
+  ok pkg update my-package`,
+	Args: cobra.MaximumNArgs(1),
 	RunE: func(cmd *cobra.Command, args []string) error {
-		err := update.Run(PackagesManifestFilename)
+		var packageName string
+		if len(args) == 1 {
+			packageName = args[0]
+		}
+
+		err := update.Run(PackagesManifestFilename, packageName)
 		if err != nil {
 			return err
+		}
+
+		if packageName != "" {
+			fmt.Printf("Updated package: %s\n", packageName)
+		} else {
+			fmt.Println("Updated all packages")
 		}
 
 		return nil

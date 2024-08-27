471a7689048d2b84afb274dd9eae7dc0146974d0
Anders K. Pettersen <anders@const.dev>
2024-08-27 12:51:55 +0200

    feat: Allow updating single package with `ok pkg update <package-name>` (#198)
    Co-authored-by: Anders K. Pettersen <staticaland@users.noreply.github.com>

--- pkg/pkg/update/update.go
+++ pkg/pkg/update/update.go
@@ -7,7 +7,7 @@ import (
 	"github.com/oslokommune/ok/pkg/pkg/githubreleases"
 )
 
-func Run(pkgManifestFilename string) error {
+func Run(pkgManifestFilename string, packageName string) error {
 	manifest, err := common.LoadPackageManifest(pkgManifestFilename)
 	if err != nil {
 		return fmt.Errorf("loading package manifest: %w", err)
@@ -18,14 +18,37 @@ func Run(pkgManifestFilename string) error {
 		return fmt.Errorf("getting latest releases: %w", err)
 	}
 
-	// Set each package to the latest release
-	for i, pkg := range manifest.Packages {
-		manifest.Packages[i].Ref = fmt.Sprintf("%s-%s", pkg.Template, latestReleases[pkg.Template])
+	if packageName != "" {
+		// Update only the specified package
+		updated := false
+		for i, pkg := range manifest.Packages {
+			if pkg.Template == packageName {
+				latestRelease, ok := latestReleases[pkg.Template]
+				if !ok {
+					return fmt.Errorf("no latest release found for package: %s", packageName)
+				}
+				manifest.Packages[i].Ref = fmt.Sprintf("%s-%s", pkg.Template, latestRelease)
+				updated = true
+				break
+			}
+		}
+		if !updated {
+			return fmt.Errorf("package not found: %s", packageName)
+		}
+	} else {
+		// Update all packages
+		for i, pkg := range manifest.Packages {
+			latestRelease, ok := latestReleases[pkg.Template]
+			if !ok {
+				return fmt.Errorf("no latest release found for package: %s", pkg.Template)
+			}
+			manifest.Packages[i].Ref = fmt.Sprintf("%s-%s", pkg.Template, latestRelease)
+		}
 	}
 
 	err = common.SavePackageManifest(pkgManifestFilename, manifest)
 	if err != nil {
-		return err
+		return fmt.Errorf("saving package manifest: %w", err)
 	}
 
 	return nil

793e9f5636bb96052b26c21e3c75e0b7e9e339a9
Jarle Fosen <jarlefosen@gmail.com>
2024-09-02 16:14:58 +0200

    feat: ok pkg tab-completions

--- cmd/pkg/common.go
+++ cmd/pkg/common.go
@@ -1,3 +1,14 @@
 package pkg
 
 const PackagesManifestFilename = "packages.yml"
+
+// argsContainsElement checks if an element is in an array which
+// is used in the tab completion functions to filter out already selected elements
+func argsContainsElement[T comparable](arr []T, element T) bool {
+	for _, e := range arr {
+		if e == element {
+			return true
+		}
+	}
+	return false
+}

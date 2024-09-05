f44cd83a0df98f8841bab9047cd88129da7ed7e7
Anders K. Pettersen <staticaland@users.noreply.github.com>
2024-09-05 14:07:22 +0200

    feat: Add docs command to generate Markdown documentation

--- magefiles/magefile.go
+++ magefiles/magefile.go
@@ -5,7 +5,11 @@ package main
 import (
 	"context"
 	"fmt"
+	"io/ioutil"
 	"os"
+	"path/filepath"
+	"regexp"
+	"strings"
 
 	"dagger.io/dagger"
 	"github.com/magefile/mage/sh"
@@ -15,6 +19,50 @@ import (
 // Build the project.
 func Build() error {
 	return sh.RunV("go", "build", "-o", "main", "main.go")
+}
+
+func Docs() error {
+	// Step 1: Remove the docs directory
+	if err := os.RemoveAll("docs"); err != nil {
+		return err
+	}
+
+	// Step 2: Create the docs directory
+	if err := os.Mkdir("docs", os.ModePerm); err != nil {
+		return err
+	}
+
+	// Step 3: Generate the docs again
+	if err := sh.RunV("go", "run", "main.go", "docs"); err != nil {
+		return err
+	}
+
+	// Step 4: Format the docs to make them more readable
+	err := filepath.Walk("docs", func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		if !info.IsDir() && strings.HasSuffix(path, ".md") {
+			content, err := ioutil.ReadFile(path)
+			if err != nil {
+				return err
+			}
+
+			text := string(content)
+			text = regexp.MustCompile(`SEE ALSO`).ReplaceAllString(text, "See also")
+			text = regexp.MustCompile(`(?m)^## `).ReplaceAllString(text, "# ")
+			text = regexp.MustCompile(`(?m)^### `).ReplaceAllString(text, "## ")
+			text = regexp.MustCompile(`(?m)^#### `).ReplaceAllString(text, "### ")
+			text = regexp.MustCompile(`(?m)^##### `).ReplaceAllString(text, "#### ")
+
+			if err := ioutil.WriteFile(path, []byte(text), info.Mode()); err != nil {
+				return err
+			}
+		}
+		return nil
+	})
+
+	return err
 }
 
 // Run the project.

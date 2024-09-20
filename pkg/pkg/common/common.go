39f1dfb490ff58ed5a600e642b373e02091831ca
Håvard Eide <h.eide@eide.org>
2024-09-20 13:59:12 +0200

    unify our consts into pkg/pkg/common/common.go to have one single place to define consts used

---
+++ pkg/pkg/common/common.go
@@ -0,0 +1,8 @@
+package common
+
+const PackagesManifestFilename = "packages.yml"
+const BoilerplateRepoOwner = "oslokommune"
+const BoilerplateRepoName = "golden-path-boilerplate"
+
+const DefaultBaseUrl = "git@github.com:oslokommune/golden-path-boilerplate.git//"
+const DefaultPackagePathPrefix = "boilerplate/terraform"

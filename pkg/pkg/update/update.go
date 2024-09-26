ebe0818a58c8dec833f84c47a2fe8b52dbc7f008
Håvard Eide <h.eide@eide.org>
2024-09-26 13:05:11 +0200

    ok pkg update - refactor to be able to test and read code more clearly

--- pkg/pkg/update/update.go
+++ pkg/pkg/update/update.go
@@ -16,6 +16,7 @@ func Run(pkgManifestFilename string, packageName string, updateConfigSchema bool
 	if err != nil {
 		return fmt.Errorf("loading package manifest: %w", err)
 	}
+
 	latestReleases, err := githubreleases.GetLatestReleases()
 	if err != nil {
 		if strings.Contains(err.Error(), "secret not found in keyring") {
@@ -24,49 +25,9 @@ func Run(pkgManifestFilename string, packageName string, updateConfigSchema bool
 		return fmt.Errorf("failed getting latest github releases: %w", err)
 	}
 
-	// Set each package to the latest release
-	updatedPackages := make([]common.Package, 0, len(manifest.Packages))
-	if packageName != "" {
-		// Update only the specified package
-		updated := false
-		for i, pkg := range manifest.Packages {
-			if pkg.OutputFolder == packageName {
-				latestRelease, ok := latestReleases[pkg.Template]
-				if !ok {
-					return fmt.Errorf("no latest release found for package: %s", packageName)
-				}
-				newRef := fmt.Sprintf("%s-%s", pkg.Template, latestRelease)
-				if manifest.Packages[i].Ref != newRef {
-					manifest.Packages[i].Ref = fmt.Sprintf("%s-%s", pkg.Template, latestRelease)
-					updatedPackages = append(updatedPackages, manifest.Packages[i])
-				}
-				updated = true
-
-				// TODO: This is a temporary fix
-				// Right now we must support multiple packages with the same output folder for GHA
-				// we can't break here - 'ok pkg update ../..' for GHA would only update the first package
-				// and we don't have any unique identifier for the package yet
-				if manifest.PackagePrefix() != common.BoilerplatePackageGitHubActionsPath {
-					break
-				}
-			}
-		}
-		if !updated {
-			return fmt.Errorf("package not found: %s", packageName)
-		}
-	} else {
-		// Update all packages
-		for i, pkg := range manifest.Packages {
-			latestRelease, ok := latestReleases[pkg.Template]
-			if !ok {
-				return fmt.Errorf("no latest release found for package: %s", pkg.Template)
-			}
-			newRef := fmt.Sprintf("%s-%s", pkg.Template, latestRelease)
-			if manifest.Packages[i].Ref != newRef {
-				manifest.Packages[i].Ref = fmt.Sprintf("%s-%s", pkg.Template, latestRelease)
-				updatedPackages = append(updatedPackages, manifest.Packages[i])
-			}
-		}
+	updatedPackagesList, err := updatedPackages(manifest, packageName, latestReleases)
+	if err != nil {
+		return err
 	}
 
 	err = common.SavePackageManifest(pkgManifestFilename, manifest)
@@ -75,32 +36,91 @@ func Run(pkgManifestFilename string, packageName string, updateConfigSchema bool
 	}
 
 	if updateConfigSchema {
-		ctx := context.Background()
-		gh, err := githubreleases.GetGitHubClient()
+		err = updateSchemaConfig(context.Background(), updatedPackagesList, manifest, latestReleases)
 		if err != nil {
-			return fmt.Errorf("getting GitHub client: %w", err)
+			return err
 		}
-		for i, pkg := range updatedPackages {
-			newRef := fmt.Sprintf("%s-%s", pkg.Template, latestReleases[pkg.Template])
-			manifest.Packages[i].Ref = newRef
+	}
 
-			configFile, ok := getLastConfigFile(pkg)
+	return nil
+}
+
+func updatedPackages(manifest common.PackageManifest, packageName string, latestReleases map[string]string) ([]common.Package, error) {
+	updatedPackagesList := make([]common.Package, 0, len(manifest.Packages))
+
+	if packageName != "" {
+		updated, err := updateSpecificPackage(manifest, packageName, latestReleases, &updatedPackagesList)
+		if err != nil {
+			return nil, err
+		}
+		if !updated {
+			return nil, fmt.Errorf("package not found: %s", packageName)
+		}
+	} else {
+		for i, pkg := range manifest.Packages {
+			latestRelease, ok := latestReleases[pkg.Template]
 			if !ok {
-				continue
+				return nil, fmt.Errorf("no latest release found for package: %s", pkg.Template)
 			}
-			downloader := githubreleases.NewFileDownloader(gh, common.BoilerplateRepoOwner, common.BoilerplateRepoName, newRef)
-			stackPath := githubreleases.GetTemplatePath(manifest.PackagePrefix(), pkg.Template)
-			schema, err := config.GenerateJsonSchemaForApp(ctx, downloader, stackPath, newRef)
-			if err != nil {
-				return fmt.Errorf("generating json schema for app: %w", err)
-			}
-
-			_, err = config.CreateOrUpdateConfigurationFile(configFile, newRef, schema)
-			if err != nil {
-				return fmt.Errorf("creating or updating configuration file: %w", err)
+			newRef := fmt.Sprintf("%s-%s", pkg.Template, latestRelease)
+			if manifest.Packages[i].Ref != newRef {
+				manifest.Packages[i].Ref = newRef
+				updatedPackagesList = append(updatedPackagesList, manifest.Packages[i])
 			}
 		}
+	}
 
+	return updatedPackagesList, nil
+}
+
+func updateSpecificPackage(manifest common.PackageManifest, packageName string, latestReleases map[string]string, updatedPackages *[]common.Package) (bool, error) {
+	updated := false
+	for i, pkg := range manifest.Packages {
+		if pkg.OutputFolder == packageName {
+			latestRelease, ok := latestReleases[pkg.Template]
+			if !ok {
+				return false, fmt.Errorf("no latest release found for package: %s", packageName)
+			}
+			newRef := fmt.Sprintf("%s-%s", pkg.Template, latestRelease)
+			if manifest.Packages[i].Ref != newRef {
+				manifest.Packages[i].Ref = newRef
+				*updatedPackages = append(*updatedPackages, manifest.Packages[i])
+			}
+			updated = true
+
+			if manifest.PackagePrefix() != common.BoilerplatePackageGitHubActionsPath {
+				break
+			}
+		}
+	}
+	return updated, nil
+}
+
+func updateSchemaConfig(ctx context.Context, updatedPackages []common.Package, manifest common.PackageManifest, latestReleases map[string]string) error {
+	gh, err := githubreleases.GetGitHubClient()
+	if err != nil {
+		return fmt.Errorf("getting GitHub client: %w", err)
+	}
+
+	for i, pkg := range updatedPackages {
+		newRef := fmt.Sprintf("%s-%s", pkg.Template, latestReleases[pkg.Template])
+		manifest.Packages[i].Ref = newRef
+
+		configFile, ok := getLastConfigFile(pkg)
+		if !ok {
+			continue
+		}
+		downloader := githubreleases.NewFileDownloader(gh, common.BoilerplateRepoOwner, common.BoilerplateRepoName, newRef)
+		stackPath := githubreleases.GetTemplatePath(manifest.PackagePrefix(), pkg.Template)
+		schema, err := config.GenerateJsonSchemaForApp(ctx, downloader, stackPath, newRef)
+		if err != nil {
+			return fmt.Errorf("generating json schema for app: %w", err)
+		}
+
+		_, err = config.CreateOrUpdateConfigurationFile(configFile, newRef, schema)
+		if err != nil {
+			return fmt.Errorf("creating or updating configuration file: %w", err)
+		}
 	}
 
 	return nil
---
+++ pkg/pkg/update/update_test.go
@@ -0,0 +1,215 @@
+package update
+
+import (
+	"testing"
+
+	"github.com/oslokommune/ok/pkg/pkg/common"
+	"github.com/stretchr/testify/require"
+)
+
+func TestUpdatedPackages(t *testing.T) {
+	tests := []struct {
+		name           string
+		manifest       common.PackageManifest
+		packageName    string
+		latestReleases map[string]string
+		expected       []common.Package
+		expectError    bool
+	}{
+		{
+			name: "update all packages",
+			manifest: common.PackageManifest{
+				Packages: []common.Package{
+					{Template: "template1", Ref: "template1-v1.0.0", OutputFolder: "folder1"},
+					{Template: "template2", Ref: "template2-v1.0.0", OutputFolder: "folder2"},
+				},
+			},
+			packageName: "",
+			latestReleases: map[string]string{
+				"template1": "v1.1.0",
+				"template2": "v1.2.0",
+			},
+			expected: []common.Package{
+				{Template: "template1", Ref: "template1-v1.1.0", OutputFolder: "folder1"},
+				{Template: "template2", Ref: "template2-v1.2.0", OutputFolder: "folder2"},
+			},
+			expectError: false,
+		},
+		{
+			name: "update specific package",
+			manifest: common.PackageManifest{
+				Packages: []common.Package{
+					{Template: "template1", Ref: "template1-v1.0.0", OutputFolder: "folder1"},
+					{Template: "template2", Ref: "template2-v1.0.0", OutputFolder: "folder2"},
+				},
+			},
+			packageName: "folder1",
+			latestReleases: map[string]string{
+				"template1": "v1.1.0",
+				"template2": "v1.2.0",
+			},
+			expected: []common.Package{
+				{Template: "template1", Ref: "template1-v1.1.0", OutputFolder: "folder1"},
+			},
+			expectError: false,
+		},
+		{
+			name: "package not found",
+			manifest: common.PackageManifest{
+				Packages: []common.Package{
+					{Template: "template1", Ref: "template1-v1.0.0", OutputFolder: "folder1"},
+				},
+			},
+			packageName: "folder2",
+			latestReleases: map[string]string{
+				"template1": "v1.1.0",
+			},
+			expected:    nil,
+			expectError: true,
+		},
+		{
+			name: "no latest release found",
+			manifest: common.PackageManifest{
+				Packages: []common.Package{
+					{Template: "template1", Ref: "template1-v1.0.0", OutputFolder: "folder1"},
+				},
+			},
+			packageName: "",
+			latestReleases: map[string]string{
+				"template2": "v1.2.0",
+			},
+			expected:    nil,
+			expectError: true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			result, err := updatedPackages(tt.manifest, tt.packageName, tt.latestReleases)
+			if tt.expectError {
+				require.Error(t, err)
+			} else {
+				require.NoError(t, err)
+				require.Equal(t, tt.expected, result)
+			}
+		})
+	}
+}
+
+func TestUpdateSpecificPackage(t *testing.T) {
+	tests := []struct {
+		name           string
+		manifest       common.PackageManifest
+		packageName    string
+		latestReleases map[string]string
+		expected       []common.Package
+		expectUpdated  bool
+		expectError    bool
+	}{
+		{
+			name: "update specific package",
+			manifest: common.PackageManifest{
+				Packages: []common.Package{
+					{Template: "template1", Ref: "template1-v1.0.0", OutputFolder: "folder1"},
+					{Template: "template2", Ref: "template2-v1.0.0", OutputFolder: "folder2"},
+				},
+			},
+			packageName: "folder1",
+			latestReleases: map[string]string{
+				"template1": "v1.1.0",
+				"template2": "v1.2.0",
+			},
+			expected: []common.Package{
+				{Template: "template1", Ref: "template1-v1.1.0", OutputFolder: "folder1"},
+			},
+			expectUpdated: true,
+			expectError:   false,
+		},
+		{
+			name: "package not found",
+			manifest: common.PackageManifest{
+				Packages: []common.Package{
+					{Template: "template1", Ref: "template1-v1.0.0", OutputFolder: "folder1"},
+				},
+			},
+			packageName: "folder2",
+			latestReleases: map[string]string{
+				"template1": "v1.1.0",
+			},
+			expected:      []common.Package{},
+			expectUpdated: false,
+			expectError:   false,
+		},
+		{
+			name: "no latest release found",
+			manifest: common.PackageManifest{
+				Packages: []common.Package{
+					{Template: "template1", Ref: "template1-v1.0.0", OutputFolder: "folder1"},
+				},
+			},
+			packageName: "folder1",
+			latestReleases: map[string]string{
+				"template2": "v1.2.0",
+			},
+			expected:      []common.Package{},
+			expectUpdated: false,
+			expectError:   true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			updatedPackages := []common.Package{}
+			updated, err := updateSpecificPackage(tt.manifest, tt.packageName, tt.latestReleases, &updatedPackages)
+			if tt.expectError {
+				require.Error(t, err)
+			} else {
+				require.NoError(t, err)
+				require.Equal(t, tt.expectUpdated, updated)
+				require.Equal(t, tt.expected, updatedPackages)
+			}
+		})
+	}
+}
+
+func TestGetLastConfigFile(t *testing.T) {
+	tests := []struct {
+		name     string
+		pkg      common.Package
+		expected string
+		ok       bool
+	}{
+		{
+			name: "package with var files",
+			pkg: common.Package{
+				VarFiles: []string{"config1.yaml", "config2.yaml"},
+			},
+			expected: "config2.yaml",
+			ok:       true,
+		},
+		{
+			name: "package with one var files",
+			pkg: common.Package{
+				VarFiles: []string{"config1.yaml"},
+			},
+			expected: "config1.yaml",
+			ok:       true,
+		},
+		{
+			name: "package without var files",
+			pkg: common.Package{
+				VarFiles: []string{},
+			},
+			expected: "",
+			ok:       false,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			result, ok := getLastConfigFile(tt.pkg)
+			require.Equal(t, tt.ok, ok)
+			require.Equal(t, tt.expected, result)
+		})
+	}
+}

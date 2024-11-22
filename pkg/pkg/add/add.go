1318df57fe34d7d5984d5d76d7633a9324fc8cf0
Sondre Engebråten <sondre.engebraten@origo.oslo.kommune.no>
2024-11-22 14:14:35 +0100

    feat: Create default config on pkg add

--- pkg/pkg/add/add.go
+++ pkg/pkg/add/add.go
@@ -7,9 +7,12 @@ import (
 	"github.com/oslokommune/ok/pkg/pkg/schema"
 	"os"
 	"strings"
+	"gopkg.in/yaml.v3"
+	"slices"
 
 	"github.com/oslokommune/ok/pkg/pkg/common"
 	"github.com/oslokommune/ok/pkg/pkg/githubreleases"
+	"github.com/oslokommune/ok/pkg/pkg/config"
 )
 
 type AddResult struct {
@@ -43,7 +46,7 @@ func Run(pkgManifestFilename string, templateName, outputFolder string, updateSc
 	if err != nil {
 		return nil, err
 	}
-
+    
 	newPackage, err := createNewPackage(manifest, templateName, gitRef, outputFolder)
 	if err != nil {
 		return nil, err
@@ -55,6 +58,10 @@ func Run(pkgManifestFilename string, templateName, outputFolder string, updateSc
 
 	manifest.Packages = append(manifest.Packages, newPackage)
 	if err := common.SavePackageManifest(pkgManifestFilename, manifest); err != nil {
+		return nil, err
+	}
+
+	if err := createDefaultConfig(ctx, gh, manifest, templateName, gitRef, outputFolder); err != nil {
 		return nil, err
 	}
 
@@ -113,6 +120,86 @@ func createNewPackage(manifest common.PackageManifest, templateName, gitRef, out
 	}
 
 	return newPackage, nil
+}
+
+type (
+	BPConfigVariable struct {
+		Name        string `yaml:"name"`
+		Default     string `yaml:"default,omitempty"`
+	}
+)
+
+func createConfigFromBoilerplate(ctx context.Context, downloader config.FileDownloader, stackPath, gitRef string, stackName string) ([]byte, error) {
+	stacks, err := config.DownloadBoilerplateStacksWithDependencies(ctx, downloader, stackPath)
+	if err != nil {
+		return []byte(""), fmt.Errorf("downloading boilerplate stacks: %w", err)
+	}
+	fmt.Println(ctx, downloader, stackPath, gitRef, stacks[0])
+
+	modules := schema.BuildModuleVariables(stacks)
+	fmt.Println(modules)
+
+	variables := make(map[string]interface{}, 0)
+
+	ignore_fields := []string{"AccountId", "Team", "Environment", "TemplateVersion", "TerraformVersion", "AwsProviderVersion", "Region"}
+
+	for _, module := range modules {
+		for _, variable := range module.Variables {
+			if slices.Contains(ignore_fields, variable.Name) {
+				continue
+			}
+
+			fmt.Println(variable)
+			//variables = append(variables, BPConfigVariable{ Name: variable.Name, Default: "default" })
+			if variable.Default == nil {
+				if variable.Name == "StackName" || variable.Name == "AppName" {
+					variables[variable.Name] = stackName
+				} else {
+					variables[variable.Name] = "<fill this in>"
+				}
+			} else {
+				variables[variable.Name] = variable.Default
+			}
+		}
+	}
+
+	defaultConfig, err := yaml.Marshal(variables)
+	if err != nil {
+		return []byte(""), fmt.Errorf("marshalling modules: %w", err)
+	}
+
+	return defaultConfig, nil
+}
+
+func createDefaultConfig(ctx context.Context, gh *github.Client, manifest common.PackageManifest, templateName, gitRef, outputFolder string) error {
+	downloader := githubreleases.NewFileDownloader(gh, common.BoilerplateRepoOwner, common.BoilerplateRepoName, gitRef)
+	stackPath := githubreleases.GetTemplatePath(manifest.PackagePrefix(), templateName)
+	defaultConfig, err := createConfigFromBoilerplate(ctx, downloader, stackPath, gitRef, outputFolder)
+	if err != nil {
+		return fmt.Errorf("generating config for: %w", err)
+	}
+	configFile := common.ConfigFile(manifest.PackageConfigPrefix(), outputFolder)
+
+	fmt.Println("Creating or updating configuration file: ", configFile)
+
+	if _, err := os.Stat(configFile); err == nil {
+		return nil
+	}
+
+	//Write to file
+	err = os.WriteFile(configFile, defaultConfig, 0644)
+	if err != nil {
+		return fmt.Errorf("writing to file: %w", err)
+	}
+
+	fmt.Println("Created configuration file: ", configFile)
+
+
+	// _, err = schema.CreateOrUpdateConfigurationFile(configFile, gitRef, generatedSchema)
+	// if err != nil {
+	// 	return fmt.Errorf("creating or updating configuration file: %w", err)
+	// }
+	return nil
 }
 
 func updateSchemaConfig(ctx context.Context, gh *github.Client, manifest common.PackageManifest, templateName, gitRef, outputFolder string) error {

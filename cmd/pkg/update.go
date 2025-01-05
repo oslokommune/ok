cb7fae955a49167c23bf327f6a3bfffdc23c2920
Yngvar Kristiansen <562343+yngvark@users.noreply.github.com>
2025-01-05 06:41:16 +0100

    feat: Add dependency injection for github releases

--- cmd/pkg/update.go
+++ cmd/pkg/update.go
@@ -11,90 +11,97 @@ import (
 	"github.com/spf13/cobra"
 )
 
-var flagDisableManifestUpdate bool
-var flagUpdateCommandUpdateSchema bool
-var flagMigrateConfig bool
+type GitHubReleases interface {
+	GetLatestReleases() (map[string]string, error)
+}
 
-func init() {
-	UpdateCommand.Flags().BoolVarP(&flagInteractive,
+func NewUpdateCommand(ghReleases GitHubReleases) *cobra.Command {
+	var flagDisableManifestUpdate bool
+	var flagUpdateCommandUpdateSchema bool
+	var flagMigrateConfig bool
+
+	updater := update.NewUpdater(ghReleases)
+
+	cmd := &cobra.Command{
+		Use:   "update [outputFolder ...]",
+		Short: "Update Boilerplate package manifest and package configuration files",
+		Long: `Update Boilerplate package manifest and package configuration files.
+
+` + InstallUpdateArgumentDescription,
+		Example: `ok pkg update
+ok pkg update my-package
+`,
+		ValidArgsFunction: updateTabCompletion,
+		RunE: func(cmd *cobra.Command, outputFolders []string) error {
+			if flagInteractive && len(outputFolders) > 0 {
+				return fmt.Errorf("cannot use both --interactive and outputFolder arguments")
+			}
+
+			var packages []common.Package
+			var err error
+
+			manifest, err := common.LoadPackageManifest(common.PackagesManifestFilename)
+			if err != nil {
+				return fmt.Errorf("loading package manifest: %w", err)
+			}
+
+			// Select packages
+			switch {
+			case len(outputFolders) > 0:
+				// Use output folders to determine which packages to install
+				packages = install.FindPackagesFromOutputFolders(manifest.Packages, outputFolders)
+
+			case flagInteractive:
+				// Use interactive mode to determine which packages to install
+				packages, err = interactive.SelectPackages(manifest, "update")
+				if err != nil {
+					return fmt.Errorf("selecting packages: %w", err)
+				}
+
+				if len(packages) == 0 {
+					fmt.Println("No packages selected. Remember to use space (or x) to select package(s) to install.")
+					return nil
+				}
+
+			default:
+				packages = manifest.Packages
+			}
+
+			opts := update.Options{
+				DisableManifestUpdate: flagDisableManifestUpdate,
+				MigrateConfig:         flagMigrateConfig,
+				UpdateSchemaConfig:    flagUpdateCommandUpdateSchema,
+			}
+
+			err = updater.Run(common.PackagesManifestFilename, packages, opts)
+			if err != nil {
+				return fmt.Errorf("updating packages: %w", err)
+			}
+
+			return nil
+		},
+	}
+
+	cmd.Flags().BoolVarP(&flagInteractive,
 		FlagInteractiveName, FlagInteractiveShorthand, false, FlagInteractiveUsage)
 
-	UpdateCommand.Flags().BoolVar(&flagDisableManifestUpdate,
+	cmd.Flags().BoolVar(&flagDisableManifestUpdate,
 		"disable-manifest-update",
 		false,
 		"Disable package manifest version updates (useful when using an external dependency manager like Renovate)",
 	)
 
-	UpdateCommand.Flags().BoolVar(&flagUpdateCommandUpdateSchema,
+	cmd.Flags().BoolVar(&flagUpdateCommandUpdateSchema,
 		"update-schema",
 		true,
 		"Update the JSON schema for affected packages")
 
-	UpdateCommand.Flags().BoolVar(&flagMigrateConfig,
+	cmd.Flags().BoolVar(&flagMigrateConfig,
 		"migrate-config",
 		true,
 		"Automatically migrate package configuration files to the latest version, if possible")
-}
 
-var UpdateCommand = &cobra.Command{
-	Use:   "update [outputFolder ...]",
-	Short: "Update Boilerplate package manifest and package configuration files",
-	Long: `Update Boilerplate package manifest and package configuration files.
-
-` + InstallUpdateArgumentDescription,
-	Example: `ok pkg update
-ok pkg update my-package
-`,
-	Args:              cobra.MaximumNArgs(1),
-	ValidArgsFunction: updateTabCompletion,
-	RunE: func(cmd *cobra.Command, outputFolders []string) error {
-		if flagInteractive && len(outputFolders) > 0 {
-			return fmt.Errorf("cannot use both --interactive and outputFolder arguments")
-		}
-
-		var packages []common.Package
-		var err error
-
-		manifest, err := common.LoadPackageManifest(common.PackagesManifestFilename)
-		if err != nil {
-			return fmt.Errorf("loading package manifest: %w", err)
-		}
-
-		// Select packages
-		switch {
-		case len(outputFolders) > 0:
-			// Use output folders to determine which packages to install
-			packages = install.FindPackagesFromOutputFolders(manifest.Packages, outputFolders)
-
-		case flagInteractive:
-			// Use interactive mode to determine which packages to install
-			packages, err = interactive.SelectPackages(manifest, "update")
-			if err != nil {
-				return fmt.Errorf("selecting packages: %w", err)
-			}
-
-			if len(packages) == 0 {
-				fmt.Println("No packages selected. Remember to use space (or x) to select package(s) to install.")
-				return nil
-			}
-
-		default:
-			packages = manifest.Packages
-		}
-
-		opts := update.Options{
-			DisableManifestUpdate: flagDisableManifestUpdate,
-			MigrateConfig:         flagMigrateConfig,
-			UpdateSchemaConfig:    flagUpdateCommandUpdateSchema,
-		}
-
-		err = update.Run(common.PackagesManifestFilename, packages, opts)
-		if err != nil {
-			return fmt.Errorf("updating packages: %w", err)
-		}
-
-		return nil
-	},
+	return cmd
 }
 
 func updateTabCompletion(cmd *cobra.Command, args []string, toComplete string) ([]string, cobra.ShellCompDirective) {

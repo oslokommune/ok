0d1f79836c0ecd27c14806f502c9659c748aa670
Yngvar Kristiansen <562343+yngvark@users.noreply.github.com>
2025-07-22 17:32:48 +0200

    newlines

--- pkg/pkg/common/save_manifest.go
+++ pkg/pkg/common/save_manifest.go
@@ -14,16 +14,19 @@ func SavePackageManifest(filePath string, manifest PackageManifest) (err error) 
 	if err := os.MkdirAll(dir, 0755); err != nil {
 		return err
 	}
+
 	// if the file already exists it will be overwritten
 	// and the file PERM will be retained
 	f, err := os.OpenFile(filePath, os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0644)
 	if err != nil {
 		return fmt.Errorf("opening manifest file '%s': %w", filePath, err)
 	}
+
 	defer f.Close()
 
 	enc := yaml.NewEncoder(f)
 	defer enc.Close()
+
 	if err := enc.Encode(manifest); err != nil {
 		return fmt.Errorf("encoding YAML manifest and write to file '%s': %w", filePath, err)
 	}

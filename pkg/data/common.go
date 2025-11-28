ab680832f8862a2150f5bb1acf02a8a4a66817ee
Sondre Engebråten <sondre.engebraten@origo.oslo.kommune.no>
2025-11-28 22:46:28 +0100

    fix: Use SSH instead of HTTPS for databricks template url

--- pkg/data/common.go
+++ pkg/data/common.go
@@ -2,8 +2,7 @@ package data
 
 const (
 	// DefaultTemplateURL is the default Oslo Kommune databricks template
-	// TODO: Update with actual template repository URL
-	DefaultTemplateURL = "github.com/oslokommune/databricks-template"
+	DefaultTemplateURL = "git@github.com:oslokommune/databricks-template"
 
 	// TemplateURLEnvName allows overriding the default template
 	TemplateURLEnvName = "DATA_TEMPLATE_URL"
